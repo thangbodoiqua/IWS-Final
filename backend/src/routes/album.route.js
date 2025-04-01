f1c168b8c9a0f98dec213c8096e60f632977b56e
Thangbodoiqua <dluy2406@gmail.com>
2025-04-01 15:20:44 +0700

    Album route logic

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,8 +1,32 @@
 import { Router } from 'express';
 
 const router = Router();
-router.get('/', (req, res) => {
-    res.send('Album route with get method')
-})
+async function getAllAlbums(req, res, next) {
+    try {
+        const albums = await Album.find();
+    
+        res.status(200).json(albums);
+    } catch (error) {
+        console.error('Error getting all albums:', error);
+        next(error);
+    }
+}
+
+async function getAllAlbumById(req, res, next) {
+    try {
+        const { albumId } = req.params
+        const album = await Album.findById(albumId).populate('songs');
+        if (!album) {
+            return res.status(404).json({ message: 'Album not found' });
+        }
+        
+        res.status(200).json(album);
+    } catch (error) {
+        console.error('Error getting album by ID:', error);
+        next(error);
+    }
+}
+router.get('/', getAllAlbums);
+router.get('/:id', getAllAlbumById);
 
 export default router;
