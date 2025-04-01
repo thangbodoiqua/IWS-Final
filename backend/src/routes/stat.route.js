ec1e152a4d699db22f1b771ad52032e4948de523
Thangbodoiqua <dluy2406@gmail.com>
2025-04-01 16:02:16 +0700

    Stats route logic

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,47 @@
 import { Router } from 'express';
+import { Song } from '../model/song.model.js';
+import { User } from '../model/user.model.js';
+import { Album } from '../model/album.model.js';
+import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js'
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Stat route with get method')
-})
+async function getStats(req, res, next) {
+    try {
+        const { totalSongs, totalUsers, totalAlbums } = await Promise.all([
+            Song.countDocuments(),
+            User.countDocuments(),
+            Album.countDocuments(),
+            Song.aggregate([
+                {
+                    $unionWith: {
+                        coll: "albums",
+                        pipeline: []
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$artist",
+                    }
+                },
+                {
+                    $count: "count"
+                }
+            ])
+        ]);
+
+        res.status(200).json({
+            totalAlbums,
+            totalSongs,
+            totalUsers,
+            totalArtists: totalArtists[0]?.count || 0,
+        })
+        
+    } catch (error) {
+        console.log("getAllStats", error);
+        next(error);
+    }
+}
+router.get('/', protectRoute, requireAdmin, getStats);
 
 export default router;
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-
+import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
+import { User } from '../model/user.model.js';
 const router = Router();
 
-router.get('/like', (req, res) => {
-    res.send('User route with get method')
-})
+async function getAllUsers(req, res, next) {
+    try {
+        const currentUserId = req.auth.userId;
+        const users = await User.find({clerkId: {$ne: currentUserId}});
+        res.status(200).json(users);
+    } catch (error) {
+        next(error);
+    }
+}
+
+router.get('/', protectRoute, requireAdmin, getAllUsers);
 
 export default router;
